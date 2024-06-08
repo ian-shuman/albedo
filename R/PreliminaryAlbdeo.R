858e51da178e25518bc39851487f89c7c3f4e5a0
Ian Shuman <shuman95100@gmail.com>
2024-06-08 13:28:50 -0400

    Fixed bug related to cropping rasters during data preparation; added more boxplots

--- R/PreliminaryAlbdeo.R
+++ R/PreliminaryAlbdeo.R
@@ -31,38 +31,39 @@ breakpoints <- rast("/Users/anncrumlish/Downloads/albedo analysis/step4_smoothed
 
 
 #Define the minimum extent to which we should crop all rasters
-summer_extent <- ext(summer_albedo)
-winter_extent <- ext(winter_albedo)
-chm_extent <- ext(canopy_height_model)
-pft_extent <- ext(pft)
-tri_extent <- ext(tri)
-final_extent <- ext(chm_extent[1], pft_extent[2], chm_extent[3], tri_extent[4])
+min_extent <- summer_albedo
+min_extent <- crop(min_extent, ext(winter_albedo))
+min_extent <- crop(min_extent, ext(canopy_height_model))
+min_extent <- crop(min_extent, ext(pft))
+min_extent <- crop(min_extent, ext(tri))
+min_extent <- crop(min_extent, ext(breakpoints))
 
-#Crop rasters
-ext(summer_albedo) <- ext(final_extent)
-ext(winter_albedo) <- ext(final_extent)
-ext(fcover) <- ext(final_extent)
-ext(pft) <- ext(final_extent)
-ext(canopy_height_model) <- ext(final_extent)
-ext(topo) <- ext(final_extent)
-ext(tpi) <- ext(final_extent)
-ext(tri) <- ext(final_extent)
-ext(slope) <- ext(final_extent)
-ext(twi) <- ext(final_extent)
-ext(aspect) <- ext(final_extent)
-ext(breakpoints) <- ext(final_extent)
-ext(breakpoints1) <- ext(final_extent)
-names(fcover) <- c("Spruce_fcover" , "Alder_fcover" ,"Willow_fcover" , "OtherTall_fcover",  "LowShrubs_fcover" , "DwarfShrubs_fcover" ,"EvergreenShrubs_fcover" , "Forb_fcover" ,"DryGrass_fcover" , "WetGrass_fcover","Moss_fcover" ,"Lichen_fcover","NPV_fcover")
-vegstack <- c(summer_albedo, winter_albedo, fcover, pft) #group together all layers with the same res/extent
-envistack <- c(canopy_height_model, topo, tpi, tri, slope, aspect, twi) #group together all layers with the same res/extent
-breakpoints < breakpoints #placeholder to keep track, we need to do the same procedure on breakpoints as we do on envistack
+# Crop the rasters to the minimum extent
+summer_albedo.c <- terra::crop(summer_albedo, min_extent)
+winter_albedo.c <- terra::crop(winter_albedo, min_extent)
+canopy_height_model.c <- terra::crop(canopy_height_model, min_extent)
+aspect.c <- terra::crop(aspect, min_extent)
+slope.c <- crop(slope, min_extent)
+topo.c <- crop(topo, min_extent)
+tpi.c <- crop(tpi, min_extent)
+tri.c <- crop(tri, min_extent)
+fcover.c <- crop(fcover, min_extent)
+pft.c <- crop(pft, min_extent)
+twi.c <- crop(twi, min_extent)
+breakpoints1.c <- crop(breakpoints1, min_extent)
+breakpoints.c <- crop(breakpoints, min_extent)
+
+names(fcover.c) <- c("Spruce_fcover" , "Alder_fcover" ,"Willow_fcover" , "OtherTall_fcover",  "LowShrubs_fcover" , "DwarfShrubs_fcover" ,"EvergreenShrubs_fcover" , "Forb_fcover" ,"DryGrass_fcover" , "WetGrass_fcover","Moss_fcover" ,"Lichen_fcover","NPV_fcover")
+vegstack <- c(summer_albedo.c, winter_albedo.c, fcover.c, pft.c) #group together all layers with the same res/extent
+envistack <- c(canopy_height_model.c, topo.c, tpi.c, tri.c, slope.c, aspect.c, twi.c) #group together all layers with the same res/extent
+breakpoints.c <- breakpoints.c #placeholder to keep track, we need to do the same procedure on breakpoints as we do on envistack
 
 envistack2 <- resample(envistack, vegstack) #maybe we need to put everything in the geometry of vegstack, so that fcovers still add up to 1, fcover and albdeo are our "most important" data
-breakpoints2 <- resample(breakpoints, vegstack)
-breakpoints1 <- resample(breakpoints1, vegstack)
+breakpoints2 <- resample(mean(breakpoints), vegstack, method = "near")
+breakpoints1 <- resample(mean(breakpoints1), vegstack)
 stack <- c(envistack2, vegstack, breakpoints1, breakpoints2)
 stack_df <- as.data.frame(stack, xy = TRUE)
-
+colnames(stack_df) <- c("x", "y" , "council_watershed_chm_30m", "council_watershed_topo_30m", "council_watershed_tpi_30m" , "council_watershed_tri_30m", "council_watershed_slope_30m", "council_watershed_aspect_30m", "twi", "council_watershed_albedo_summer", "council_watershed_albedo_winter", "Spruce_fcover", "Alder_fcover" , "Willow_fcover", "OtherTall_fcover" , "LowShrubs_fcover", "DwarfShrubs_fcover", "EvergreenShrubs_fcover", "Forb_fcover" , "DryGrass_fcover", "WetGrass_fcover", "Moss_fcover", "Lichen_fcover", "NPV_fcover", "category", "break_point1_30m", "break_point_30m")
 
 
 
@@ -275,17 +276,18 @@ boxplot(break_point_30m ~ levels,data=stack_df |> mutate(levels = cut(council_wa
 title("Changes in breakpoint with changing canopy height for all PFTs")
 
 All_box <- stack_df %>%
-  mutate(levels = cut(council_watershed_chm_30m, seq(0, 12, 1))) %>%
+  mutate(levels = cut(council_watershed_chm_30m, seq(0, 12, .5))) %>%
   drop_na(levels, break_point_30m) %>%
   ggplot(aes(x = levels, y = break_point_30m)) +
   geom_boxplot(fill = "gray") +
+  scale_y_continuous(limits = c(120, 270), breaks = seq(120, 270, by = 10))+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   labs(x = "Binned Canopy Height (m)", y = "Completion of Snowmelt (DOY)")+
   theme_classic() +
   theme(axis.text = element_text(size=21, color = 'black'),
         axis.title=element_text(size=25),
         axis.text.x = element_text(angle = 45, hjust = 1))
-All_box-start <- stack_df %>%
+All_box_start <- stack_df %>%
   mutate(levels = cut(council_watershed_chm_30m, seq(0, 12, 1))) %>%
   drop_na(levels, break_point_30m) %>%
   ggplot(aes(x = levels, y = break_point1_30m)) +
@@ -315,6 +317,7 @@ DTSA_box <- stack_alder %>%
   drop_na(levels, break_point_30m) %>%
   ggplot(aes(x = levels, y = break_point_30m)) +
   geom_boxplot(fill = '#008000') +
+  scale_y_continuous(limits = c(120, 180))+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   labs(x = "Binned Canopy Height (m)", y = "Completion of Snowmelt (DOY)", title = "Completion of Snowmelt v.s. Canopy Height for Alder Dominated Sites")+
   theme_classic()
@@ -331,6 +334,7 @@ DTSW_box <- stack_willow %>%
   drop_na(levels, break_point_30m) %>%
   ggplot(aes(x = levels, y = break_point_30m)) +
   geom_boxplot(fill = '#00CD00') +
+  scale_y_continuous(limits = c(120, 180))+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   labs(x = "Binned Canopy Height (m)", y = "Completion of Snowmelt (DOY)", title = "Completion of Snowmelt v.s. Canopy Height for Willow Dominated Sites")+
   theme_classic()
@@ -347,6 +351,7 @@ ET_box <- stack_ET %>%
   drop_na(levels, break_point_30m) %>%
   ggplot(aes(x = levels, y = break_point_30m)) +
   geom_boxplot(fill = '#FF0000') +
+  scale_y_continuous(limits = c(120, 180))+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   labs(x = "Binned Canopy Height (m)", y = "Completion of Snowmelt (DOY)", title = "Completion of Snowmelt v.s. Canopy Height for Spruce Dominated Sites")+
   theme_classic()
@@ -355,10 +360,32 @@ DT_box <- stack_DT %>%
   drop_na(levels, break_point_30m) %>%
   ggplot(aes(x = levels, y = break_point_30m)) +
   geom_boxplot(fill = '#669999') +
+  scale_y_continuous(limits = c(120, 180))+
+  scale_x_binned()
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   labs(x = "Binned Canopy Height (m)", y = "Completion of Snowmelt (DOY)", title = "Completion of Snowmelt v.s. Canopy Height for Other Tall Shrub Dominated Sites")+
   theme_classic()
-plot_grid(All_box, nonwoody_box, ET_box, DT_box, DTSA_box, DTSW_box, ncol = 2)
+plot_grid(ET_box, DT_box, DTSA_box, DTSW_box, ncol = 2, align = "hv")
+
+
+stack_DG <- stack_df |> subset(stack_df$DryGrass_fcover > 0.75)
+DG_box <- stack_DG %>%
+  mutate(levels = cut(council_watershed_chm_30m, seq(0, 12, 0.5))) %>%
+  drop_na(levels, break_point_30m) %>%
+  ggplot(aes(x = levels, y = break_point_30m)) +
+  geom_boxplot(fill = '#008000') +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  labs(x = "Binned Canopy Height (m)", y = "Completion of Snowmelt (DOY)", title = "Completion of Snowmelt v.s. Canopy Height for Dry Graminoid Dominated Sites")+
+  theme_classic()
+stack_WG <- stack_df |> subset(stack_df$WetGrass_fcover > 0.75)
+WG_box <- stack_WG %>%
+  mutate(levels = cut(council_watershed_chm_30m, seq(0, 12, 0.5))) %>%
+  drop_na(levels, break_point_30m) %>%
+  ggplot(aes(x = levels, y = break_point_30m)) +
+  geom_boxplot(fill = '#008000') +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  labs(x = "Binned Canopy Height (m)", y = "Completion of Snowmelt (DOY)", title = "Completion of Snowmelt v.s. Canopy Height for Wet Graminoid Dominated Sites")+
+  theme_classic()
 
 tall_df = filter(stack_df, council_watershed_chm_30m > 5)
 mean(tall_df$break_point_30m)
@@ -498,3 +525,42 @@ correlation_matrix <- cor(sub_df[,2:7])
 
 # Create the correlation plot
 corrplot(correlation_matrix, method = "circle", type = "lower", diag = F)
+
+
+#Breakpoint DOY and fcover
+
+stack_alder_fcover <- as.data.frame(cbind(stack_df$Alder_fcover, stack_df$break_point_30m))
+colnames(stack_alder_fcover) <- c("fcover", "DOY")
+stack_alder_fcover <- na.omit(stack_alder_fcover)
+DTSA_fbox <- stack_alder_fcover %>%
+  mutate(levels = cut(fcover, seq(0, 1, 0.05))) %>%
+  drop_na(levels, DOY) %>%
+  ggplot(aes(x = levels, y = DOY)) +
+  geom_boxplot(fill = '#008000') +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  labs(x = "Binned Alder Fractional Cover (%)", y = "Completion of Snowmelt (DOY)", title = "Completion of Snowmelt v.s. Fractional Cover of Alders")+
+  theme_classic()
+
+stack_willow_fcover <- as.data.frame(cbind(stack_df$Willow_fcover, stack_df$break_point_30m))
+colnames(stack_willow_fcover) <- c("fcover", "DOY")
+stack_willow_fcover <- na.omit(stack_willow_fcover)
+DTSW_fbox <- stack_willow_fcover %>%
+  mutate(levels = cut(fcover, seq(0, 1, 0.05))) %>%
+  drop_na(levels, DOY) %>%
+  ggplot(aes(x = levels, y = DOY)) +
+  geom_boxplot(fill = "#00CD00") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  labs(x = "Binned Willow Fractional Cover (%)", y = "Completion of Snowmelt (DOY)", title = "Completion of Snowmelt v.s. Fractional Cover of Alders")+
+  theme_classic()
+
+stack_spruce_fcover <- as.data.frame(cbind(stack_df$Spruce_fcover, stack_df$break_point_30m))
+colnames(stack_spruce_fcover) <- c("fcover", "DOY")
+stack_spruce_fcover <- na.omit(stack_spruce_fcover)
+ET_fbox <- stack_spruce_fcover %>%
+  mutate(levels = cut(fcover, seq(0, 1, 0.05))) %>%
+  drop_na(levels, DOY) %>%
+  ggplot(aes(x = levels, y = DOY)) +
+  geom_boxplot(fill = "#FF0000") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  labs(x = "Binned Willow Fractional Cover (%)", y = "Completion of Snowmelt (DOY)", title = "Completion of Snowmelt v.s. Fractional Cover of Alders")+
+  theme_classic()
